136233683eee96f3d883a2ddc7827a580823f970
msquid <seokmin1002@gmail.com>
2019-06-26 20:39:24 +0900

    Update key.js

--- games/key.js
+++ games/key.js
@@ -7,7 +7,7 @@ function toEncrypt(str){
   for (i=0; i<arr.length; i++){
     encrypted += arr[i].charCodeAt(0);
   }
-  encrypted = (encrypted-Math.floor((i^3)+(((i+1)^2)/i)))*i + i^i;
+  encrypted = (encrypted-Math.floor((i^3)+(((i+1)^2)/i)))*(i^(5^i)) + i^(i^i);
   en_and_key[0] = encrypted;
   en_and_key[1] = i;
   return en_and_key;
